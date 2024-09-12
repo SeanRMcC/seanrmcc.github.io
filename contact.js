2f9d21fe570810bdda6ea641d467e0464be4dae5
Sean McCarty <soxfansean@gmail.com>
2024-09-12 19:09:25 -0400

    Add message when email is coppied to clipboard

--- contact.js
+++ contact.js
@@ -1,5 +1,12 @@
 const email = document.querySelector(".email")
 
 email.addEventListener("click", () => {
-    navigator.clipboard.writeText("mccartys@bu.edu")
+    navigator.clipboard.writeText("mccartys@bu.edu").then(
+        () => {
+            email.textContent = "email copied to clipboard"
+            setTimeout(() => {
+                email.textContent = "mccartys@bu.edu"
+            }, 300)
+        }
+    )
 })
