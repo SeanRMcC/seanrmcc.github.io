24f42a5cc540264d9f41fc488b51ab6292f99e5d
Sean McCarty <soxfansean@gmail.com>
2024-09-12 19:29:12 -0400

    Add image hover effect

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ console.log(image)
 image.addEventListener("click", () => {
     const split_string = image.src.split("/")
     const image_file = split_string[split_string.length - 1]
-    console.log(image_file)
     if(image_file == "me.jpg") {
         split_string[split_string.length - 1] = "cappy.jpg"
     } else {
