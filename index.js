58b27fea35f8e58cef7690139ac736c2eebf7eec
Sean McCarty <soxfansean@gmail.com>
2024-09-12 20:33:48 -0400

    Add more random images

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const image = document.querySelector("#me")
 console.log(image)
 
 image.addEventListener("click", () => {
+
+    pictures = ["cappy.jpg", "bingus.webp", "pibble.jpeg"]
+
     const split_string = image.src.split("/")
     const image_file = split_string[split_string.length - 1]
     if(image_file == "me.jpg") {
-        split_string[split_string.length - 1] = "cappy.jpg"
+        const randomPicture = pictures[Math.floor(Math.random() * pictures.length)];
+        console.log(randomPicture)
+        split_string[split_string.length - 1] = randomPicture
     } else {
         split_string[split_string.length - 1] = "me.jpg"
     }
