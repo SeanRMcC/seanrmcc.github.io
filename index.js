25c4d68a1d3e3f2d686f72d79495c58f94763512
Sean McCarty <soxfansean@gmail.com>
2024-09-13 08:18:07 -0400

    Reverted to previous secret

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ console.log(image)
 
 image.addEventListener("click", () => {
 
-    pictures = ["cappy.jpg", "bingus.webp", "pibble.jpeg"]
-
     const split_string = image.src.split("/")
     const image_file = split_string[split_string.length - 1]
     if(image_file == "me.jpg") {
-        const randomPicture = pictures[Math.floor(Math.random() * pictures.length)];
-        console.log(randomPicture)
-        split_string[split_string.length - 1] = randomPicture
+        split_string[split_string.length - 1] = "cappy.jpg"
     } else {
         split_string[split_string.length - 1] = "me.jpg"
     }
